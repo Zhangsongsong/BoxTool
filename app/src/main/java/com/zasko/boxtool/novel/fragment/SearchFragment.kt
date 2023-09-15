edd3c6300ce5d611a806706879f940ba4a45effd
zhangsongzhou <zhangsongzhou@inke.cn>
2023-09-15 18:57:42 +0800

    1：代码增加  + 12

--- app/src/main/java/com/zasko/boxtool/novel/fragment/SearchFragment.kt
+++ app/src/main/java/com/zasko/boxtool/novel/fragment/SearchFragment.kt
@@ -5,8 +5,15 @@ import android.view.ViewGroup
 import androidx.viewbinding.ViewBinding
 import com.zasko.boxtool.base.fragment.BaseFragment
 import com.zasko.boxtool.databinding.NovelFragmentSearchBinding
+import com.zasko.boxtool.helper.LogUtil
+import com.zasko.boxtool.utils.onClick
 
 class SearchFragment : BaseFragment() {
+
+
+    companion object {
+        private const val TAG = "SearchFragment"
+    }
 
 
     private lateinit var viewBinding: NovelFragmentSearchBinding
@@ -15,4 +22,18 @@ class SearchFragment : BaseFragment() {
         viewBinding = NovelFragmentSearchBinding.inflate(inflater, container, false)
         return viewBinding
     }
+
+
+    override fun initView() {
+        super.initView()
+        viewBinding.searchIv.onClick {
+            val text = viewBinding.searchEdit.text
+            startSearch(key = text?.toString() ?: "")
+        }
+    }
+
+    private fun startSearch(key: String) {
+        LogUtil.dPrintln("$TAG startSearch:${key}")
+    }
+
 }
